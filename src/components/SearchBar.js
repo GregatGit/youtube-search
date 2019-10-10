a8281a423aadc8f3280442b5b590980dd6e40e2f
gregatgit <gregdd@outlook.com>
2019-10-10 15:19:51 +1000

    search by term and put in state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ export default class SearchBar extends Component {
 
   onFormSubmit = e => {
     e.preventDefault()
-    console.log(this.state.term)
+    this.props.onFormSubmit(this.state.term)
   }
 
   render() {
