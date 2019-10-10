c820d009be0ecdc72f436837b0c93b406a65fb1b
gregatgit <gregdd@outlook.com>
2019-10-10 15:03:22 +1000

    axios func

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBW-87FN6tWlBA-ufgNMu_6_m5mhtZ3oH4'
+
+export default axios.create({
+  baseURL: `https://www.googleapis.com/youtube/v3/`,
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+})
+
